c3ea4599b8c18aabe8778999c72cec240420cfc7
whatsyourgithub <39016062+whatsyourgithub@users.noreply.github.com>
2018-09-26 13:00:54 -0700

    added example mobile config

---
+++ mobile-config.js
@@ -0,0 +1,21 @@
+App.info({
+    id: 'com.sdf.sdf.sdf',
+    version: '0.0.1',
+    name: 'Example app name',
+    description: 'some example description',
+    author: 'bob dole',
+    email: 'no@no.no'
+});
+
+// App.icons({
+//     'iphone_2x': 'icons/icon-60@2x.png',
+//     'iphone_3x': 'icons/icon-60@3x.png'
+// });
+
+// App.launchScreens({
+//     'iphone5': 'splash/Default~iphone5.png'
+// });
+
+// App.setPreference('BackgroundColor', '0xff0000ff');
+
+// App.configurePlugin({lkjsldjfoi3jwoi32h4})
